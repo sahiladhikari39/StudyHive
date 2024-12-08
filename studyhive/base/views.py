3525526701ea67eb36a5a885551433863cec25d7
Sahil Adhikari <sahiladhi39@gmail.com>
2024-12-08 20:09:19 +0545

    added an app and also few templates to ensure django is running

---
+++ studyhive/base/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def lobby(request):
+    return render(request, 'base/lobby.html')
+
+
+def room(request):
+    return render(request, 'base/room.html')
+
+
