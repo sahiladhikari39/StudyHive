276967b36c826c78783d8ef3727d3b0964ed6cf6
Sahil Adhikari <sahiladhi39@gmail.com>
2024-12-28 19:14:41 +0545

    adding more users

--- studyhive/static/js/streams.js
+++ studyhive/static/js/streams.js
@@ -1,6 +1,5 @@
 const APP_ID = '21dff23eba54435587405ae622d99e72'
 const CHANNEL = 'main'
-const TOKEN = '007eJxTYBAJv3M3Yv3Xi7qz7RZ5qLH8+HiwsTVsgm+Rx/TQx6asCyMVGIwMU9LSjIxTkxJNTUyMTU0tzE0MTBNTzYyMUiwtU82NBPdkpzcEMjLwdl5lYmSAQBCfhSE3MTOPgQEAQ6Qerw=='
 
 let UID;
 
@@ -10,6 +9,8 @@ let localTracks = []
 let remoteUsers = {}
 
 let joinAndDisplayLocalStream = async () => {
+
+    client.on('user-published', handleUserJoined)
     UID = await client.join(APP_ID, CHANNEL, TOKEN, null)
 
     localTracks = await AgoraRTC.createMicrophoneAndCameraTracks()
@@ -20,8 +21,37 @@ let joinAndDisplayLocalStream = async () => {
                 </div>`
 
     document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
+
     localTracks[1].play(`user-${UID}`)
     await client.publish([localTracks[0], localTracks[1]])
     
 }
+
+let handleUserJoined = async (user, mediaType) => {
+    remoteUsers[user.uid] = user
+    await client.subscribe(user, mediaType)
+    
+    if(mediaType === 'video'){
+        let player = document.getElementById(`user-container-${user.uid}`)
+        if(player != null){
+            player.remove()
+        }
+
+
+        player = `<div class="video-container" id="user-container-${user.uid}">
+                    <div class="username-wrapper"><span class="user-name">My Name</span></div>
+                    <div class="video-player" id="user-${user.uid}"></div>
+                </div>`
+
+        document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
+        user.videoTrack.play(`user-${user.uid}`)
+
+
+    }
+
+    if(mediaType === 'audio'){
+        user.audioTrack.play()
+    }
+}
+
 joinAndDisplayLocalStream()
